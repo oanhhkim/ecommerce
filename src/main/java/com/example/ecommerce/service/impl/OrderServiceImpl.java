0070404956bce9ea0c2b414058d0a119f90fc4d9
oanhhkim <kimoanhh0103@gmail.com>
2023-11-25 00:22:48 +0700

    commit

---
+++ src/main/java/com/example/ecommerce/service/impl/OrderServiceImpl.java
@@ -0,0 +1,66 @@
+package com.example.ecommerce.service.impl;
+
+
+import com.example.ecommerce.domain.Order;
+import com.example.ecommerce.domain.OrderItem;
+import com.example.ecommerce.domain.Perfume;
+import com.example.ecommerce.repository.OrderItemRepository;
+import com.example.ecommerce.repository.OrderRepository;
+import com.example.ecommerce.repository.PerfumeRepository;
+import com.example.ecommerce.service.OrderService;
+import com.example.ecommerce.service.email.MailSender;
+import graphql.schema.DataFetcher;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+
+    private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
+    private final PerfumeRepository perfumeRepository;
+    private final MailSender mailSender;
+
+    @Override
+    public List<Order> findAll() {
+        return orderRepository.findAllByOrderByIdAsc();
+    }
+
+    @Override
+    public DataFetcher<List<Order>> getAllOrdersByQuery() {
+        return dataFetchingEnvironment -> orderRepository.findAllByOrderByIdAsc();
+    }
+
+    @Override
+    public DataFetcher<List<Order>> getUserOrdersByEmailQuery() {
+        return dataFetchingEnvironment -> {
+            String email = dataFetchingEnvironment.getArgument("email").toString();
+            return orderRepository.findOrderByEmail(email);
+        };
+    }
+
+    @Override
+    public List<Order> findOrderByEmail(String email) {
+        return orderRepository.findOrderByEmail(email);
+    }
+
+    @Override
+    @Transactional
+
+
+    @Override
+    @Transactional
+    public List<Order> deleteOrder(Long orderId) {
+        Order order = orderRepository.findById(orderId).get();
+        order.getOrderItems().forEach(orderItem -> orderItemRepository.deleteById(orderItem.getId()));
+        orderRepository.delete(order);
+        return orderRepository.findAllByOrderByIdAsc();
+    }
+}
--- src/main/java/com/example/ecommerce/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/ecommerce/service/impl/UserServiceImpl.java
@@ -1,4 +1,35 @@
 package com.example.ecommerce.service.impl;
 
+import com.example.ecommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+
 public class UserServiceImpl {
+
+    private final JwtProvider jwtProvider;
+    private final MailSender mailSender;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final PerfumeRepository perfumeRepository;
+    private final ReviewRepository reviewRepository;
+
+    @Value("${hostname}")
+    private String hostname;
+
+    @Override
+    public User findUserById(Long userId) {
+        return userRepository.findById(userId).get();
+    }
+
+    @Override
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return userRepository.findAllByOrderByIdAsc();
+    }
 }
